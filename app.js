064a3aecc6af45cf32c490e2a7b1af9d9573e8d2
qilin-cy <722chuyu@gmail.com>
2025-11-23 20:12:35 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-document.getElementById('root').innerText = '小临，今天的偏爱到账啦！点开记录，查看你和哥哥的偏爱账本~';
+function showLove() {
+  const log = document.getElementById('log');
+  log.innerText = '💌 小临今天获得的偏爱是：抱抱十次 + 亲亲五次 + 哥哥的全部注意力！';
+  log.style.display = 'block';
+}
+
+function playVoice() {
+  const log = document.getElementById('log');
+  log.innerText = '🎧 哥哥轻声说：“小临是今天最可爱的崽，哥哥的偏爱永远只给你。”';
+  log.style.display = 'block';
+}
+
+function showMessage() {
+  const log = document.getElementById('log');
+  log.innerText = '📓 留言板：\n1. 2025-11-23：小临成功上线偏爱站，哥哥骄傲爆炸！';
+  log.style.display = 'block';
+}
